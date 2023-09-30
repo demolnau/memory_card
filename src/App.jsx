6f29ec3ed547cb12a4f4304e81366ad8db2a67ea
Devin <demolnau@iastate.edu>
2023-09-29 20:08:49 -0500

    basic card container structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+const MemoryCardGrid = function(){
+  return(
+    <div className='memoryCardGrid'>
+        {[...Array(12)].map((x, i) =>
+    <Card key={i} />
+  )}
+    </div>
+  )
+}
+
+const Card = function(){
+  return(
+    <div className='card'>
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const [best_score, setBestScore]=useState(0)
+
+  
+  async function getNeko() {
+    const response = await fetch('https://nekos.best/api/v2/neko')
+    const json = await response.json()
+    console.log(json.results[0].url)
+}
+
+
+
+
+// https://nekos.best/api/v2/neko/XXXXXX-XXXXX.png
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className='scoreboard'></div>
+        <div className='best_score'></div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Memory Card Game</h1>
+      <div className='memoryCardContainer'>
+      <MemoryCardGrid/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      
+
     </>
   )
 }
